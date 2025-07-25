b6763cc310ee2dc680e9de0aa72ff97afba1e130
Abdullahi muhammad <abdulzaifada81@gmail.com>
2025-07-25 06:37:35 -0700

    static deplayment

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This is the console for AirBnB"""
+import re
+import cmd
+from models import storage
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from shlex import split
+
+
+class HBNBCommand(cmd.Cmd):
+    """this class is entry point of the command interpreter
+    """
+    prompt = "(hbnb) "
+    all_classes = {"BaseModel", "User", "State", "City",
+                   "Amenity", "Place", "Review"}
+
+    def emptyline(self):
+        """Ignores empty spaces"""
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
